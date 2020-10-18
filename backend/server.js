173493001d7206198bfece6e05ecd2aba637a57c
mohammedshamia <mohdkarera@gmail.com>
2020-10-18 11:58:04 +0300

    set models

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import products from "./data/products.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
 
 const app = express();
-dotenv.config();
+dotenv.config({ path: "backend/.env" });
+connectDB();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/get/api/products", (req, res) => {
   res.json(products);
@@ -19,4 +24,7 @@ app.get("/get/api/products/:id", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`server running on port ${port}`));
+app.listen(
+  port,
+  console.log(`server running on port ${port}`.yellow.underline)
+);
