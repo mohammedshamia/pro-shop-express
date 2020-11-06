477d15a4ffeae86523ef40fe585b7601d426f02f
mohammedshamia <mohdkarera@gmail.com>
2020-11-06 18:21:30 +0200

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,17 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/");
+}
 
 const port = process.env.PORT || 5000;
 
