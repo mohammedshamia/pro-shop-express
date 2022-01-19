c0125f958084feb7abd3eca9ea2660345de7b26b
mohammedshamia <mohdkarera@gmail.com>
2022-01-19 17:38:27 +0200

    Bug fixing

--- src/data/users.js
+++ src/data/users.js
@@ -2,9 +2,27 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    firstName: "Admin",
+    firstName: "Admin1",
     lastName: "User",
-    email: "admin@example.com",
+    email: "admin1@example.com",
+    profileImage:
+      "https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg",
+    password: bcrypt.hashSync("123456PAs@", 10),
+    isAdmin: true,
+  },
+  {
+    firstName: "Admin2",
+    lastName: "User",
+    email: "admin2@example.com",
+    profileImage:
+      "https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg",
+    password: bcrypt.hashSync("123456PAs@", 10),
+    isAdmin: true,
+  },
+  {
+    firstName: "Admin3",
+    lastName: "User",
+    email: "admin3@example.com",
     profileImage:
       "https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg",
     password: bcrypt.hashSync("123456PAs@", 10),
