c06adca430186ff4bfc96cf795d88bb08067ff37
mohammedshamia <mohdkarera@gmail.com>
2022-01-25 17:30:47 +0200

    bug fixing

--- src/utils/validations/userValidations/updateProfileSchema.js
+++ src/utils/validations/userValidations/updateProfileSchema.js
@@ -18,7 +18,6 @@ export const updateProfileSchema = yup.object({
   email: yup.string().email("Email is not valid").required("Email is required"),
   password: yup
     .string()
-    .required("Password is required")
     .matches(
       /^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9!@#$%./^&*()_+<>,~`"':;]{8,}$/,
       `Password should be 8 digits length at least, contains at least one Capital letter, contains at least one number.)`
